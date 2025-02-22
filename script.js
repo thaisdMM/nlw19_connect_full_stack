ff7b2dac9081b37ae417e9646fe2a89d8e8c44f3
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-22 12:25:38 +0100

    copy content html to script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+//capturar o envio dos dados do formulário
+
+const startApp = () => {
+  const content = `
+   <form>
+      <input type="email" name="email" placeholder="E-mail" />
+      <input type="text" name="phone" placeholder="Telefone" />
+      <button>
+         Confirmar
+      </button>
+   </form>
+   `;
+}; //arrow function
+startApp(); //executada assim que o app começar
