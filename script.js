fbb001229c04f13d840e84608e1ed931b1f9f8c8
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-22 14:01:09 +0100

    added action and event to form

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 //capturar o envio dos dados do formulário
 
-const app = document.getElementById("app")
+const app = document.getElementById("app");
+
+const formAction = () => {
+  const form = document.getElementById("form");
+  //ordem de eventos
+  form.onsubmit = (event) => {
+    event.preventDefault(); //preventDefault> não faça o padrão> nesse caso era enviar o formulário quando clicasse em confirmar porque dá erro(não está configurado ainda)
+    alert("clicou");
+  };
+};
 
 //arrow function
 const startApp = () => {
   const content = `
-   <form>
+   <form id="form">
       <input type="email" name="email" placeholder="E-mail" />
       <input type="text" name="phone" placeholder="Telefone" />
       <button>
          Confirmar
       </button>
    </form>
-   `
+   `;
 
-   app.innerHTML = content //innerHTML> saber o que tem dentro ou atribuir outro valor lá para dentro
+  app.innerHTML = content; //innerHTML> saber o que tem dentro ou atribuir outro valor lá para dentro
+
+  formAction();
 };
+
 startApp(); //executada assim que o app começar
