c6f9daa197541293b2398faebaae0255b975b03b
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-23 11:02:01 +0100

    copy main html to js

--- script.js
+++ script.js
@@ -103,13 +103,51 @@ const updateImageLinks = () => {
 //arrow function
 const startApp = () => {
   const content = `
-   <form id="form">
-      <input type="email" name="email" placeholder="E-mail" />
-      <input type="text" name="phone" placeholder="Telefone" />
-      <button>
-         Confirmar
-      </button>
-   </form>
+    <main>
+            <section class="about">
+               <div class="section-header">
+                  <h2>
+                     Sobre o evento
+                  </h2>
+                  <span class="badge">AO VIVO</span>
+               </div>
+               
+               <p>
+                  Um evento feito por e para pessoas desenvolvedoras apaixonadas por criar soluções inovadoras e compartilhar conhecimento. Vamos mergulhar nas tendências mais recentes em desenvolvimento de software, arquitetura de sistemas e tecnologias emergentes, com palestras, workshops e hackathons.
+                  <br/><br/>
+Dias 15 a 17 de março | Das 18h às 21h | Online &amp; Gratuito 
+
+               </p>
+
+            </section>
+
+            <sections class="registration">
+               <h2>Iscrição</h2>
+
+               <form id="form">
+                  <div class="input-wrapper">
+                     <div class="input-group">
+                        <label for="email">
+                           <img src="mail.svg" alt="Email icon">
+                        </label>
+                        <input type="email" id="email" name="email" placeholder="E-mail">
+                     </div>
+
+                     <div class="input-group">
+                        <label for="phone">
+                           <img src="phone.svg" alt="Phone icon">
+                        </label>
+                        <input type="text" id="phone" name="phone" placeholder="Telefone">
+                     </div>
+                  </div>
+
+                  <button>
+                     Confirmar
+                     <img src="arrow.svg" alt="Arrow right">
+                  </button>
+               </form>
+            </section>
+         </main>
    `;
 
   app.innerHTML = content; //innerHTML> saber o que tem dentro ou atribuir outro valor lá para dentro
@@ -118,6 +156,5 @@ const startApp = () => {
   formAction();
 };
 
-//startApp(); //executada assim que o app começar - comentou para construir o html
-
+startApp(); //executada assim que o app começar
 document.getElementById("logo").onclick = () => startApp();
